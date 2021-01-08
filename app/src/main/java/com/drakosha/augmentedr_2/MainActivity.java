e2c2046c4cc8849a848d5604b7a9b107fe879042
zheng0zhou <809875104@qq.com>
2021-01-08 09:46:03 +0100

    modify button object

--- app/src/main/java/com/drakosha/augmentedr_2/MainActivity.java
+++ app/src/main/java/com/drakosha/augmentedr_2/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
     }
 
     public void chooseObject(View view) {
-        startActivity(new Intent("com.drakosha.augmentedr_2.PopUpActivity"));
+        startActivity(new Intent("com.drakosha.augmentedr_2.AR_Activity"));
     }
 
     public void chooseEnvironment(View view) {
